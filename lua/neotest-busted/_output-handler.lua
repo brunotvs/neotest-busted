26cd64cff6d4ba7b8129055e5a7f48054b077a0b
Bruno Thiago <brunothiagorvs@gmail.com>
2025-01-03 15:21:36 -0300

    style: using stylua to format the project

--- lua/neotest-busted/_output-handler.lua
+++ lua/neotest-busted/_output-handler.lua
@@ -7,36 +7,36 @@ local M = {}
 M.NEOTEST_RESULTS_LINE_MARKER = '::NEOTEST_LINE::'
 
 function M:__call(options)
-	local busted = require('busted')
-	local handler = require('busted.outputHandlers.base')()
+  local busted = require('busted')
+  local handler = require('busted.outputHandlers.base')()
 
-	handler.suiteEnd = function()
-		local error_info = {
-			pendings = handler.pendings,
-			successes = handler.successes,
-			failures = handler.failures,
-			errors = handler.errors,
-			duration = handler.getDuration(),
-		}
-		local ok, result = pcall(json.encode, error_info)
+  handler.suiteEnd = function()
+    local error_info = {
+      pendings = handler.pendings,
+      successes = handler.successes,
+      failures = handler.failures,
+      errors = handler.errors,
+      duration = handler.getDuration(),
+    }
+    local ok, result = pcall(json.encode, error_info)
 
-		io_write('\n' .. M.NEOTEST_RESULTS_LINE_MARKER)
+    io_write('\n' .. M.NEOTEST_RESULTS_LINE_MARKER)
 
-		if ok then
-			io_write(result)
-		else
-			io_write('Failed to encode test results to json: ' .. result)
-		end
+    if ok then
+      io_write(result)
+    else
+      io_write('Failed to encode test results to json: ' .. result)
+    end
 
-		io_write('\n')
-		io_flush()
+    io_write('\n')
+    io_flush()
 
-		return nil, true
-	end
+    return nil, true
+  end
 
-	busted.subscribe({ 'suite', 'end' }, handler.suiteEnd)
+  busted.subscribe({ 'suite', 'end' }, handler.suiteEnd)
 
-	return handler
+  return handler
 end
 
 M.source = debug.getinfo(1).source:sub(2)
